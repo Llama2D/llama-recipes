754f738ef83c45e553f08880e40d925fa1a293c6
Andrew Healey <doolie.healey@gmail.com>
2023-09-20 16:10:56 -0400

    Remove dead import

--- src/llama_recipes/configs/__init__.py
+++ src/llama_recipes/configs/__init__.py
@@ -4,4 +4,3 @@
 from llama_recipes.configs.peft import lora_config, llama_adapter_config, prefix_config
 from llama_recipes.configs.fsdp import fsdp_config
 from llama_recipes.configs.training import train_config
-from llama_recipes.configs.llama2d import llama2d_config
