43982aff49aa0721745150e8ee7cb96c9780aa52
Andrew Healey <doolie.healey@gmail.com>
2023-09-20 17:47:41 -0400

    Print dtype

--- src/llama_recipes/utils/train_utils.py
+++ src/llama_recipes/utils/train_utils.py
@@ -77,6 +77,7 @@ def train(model, train_dataloader,eval_dataloader, tokenizer, optimizer, lr_sche
                         batch[key] = batch[key].to(local_rank)
                     else:
                         batch[key] = batch[key].to('cuda:0')              
+                
                 loss = model(**batch).loss
                 loss = loss / gradient_accumulation_steps
                 total_loss += loss.detach().float()
