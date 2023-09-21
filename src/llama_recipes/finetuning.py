1bcdbf9838903d49f1f41f2e607d2750c8d00e9f
Andrew Healey <doolie.healey@gmail.com>
2023-09-21 01:11:54 -0400

    Stop error

--- src/llama_recipes/finetuning.py
+++ src/llama_recipes/finetuning.py
@@ -1,8 +1,6 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
-raise NotImplementedError("Haven't made finetuning work")
-
 import os
 from pkg_resources import packaging
 
