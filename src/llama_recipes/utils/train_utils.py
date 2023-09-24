0d78e22d1ce1f99639b2abc5c2c6038e4a27904d
neverSettles <christopherRSettles@gmail.com>
2023-09-24 14:45:58 -0700

    Make it so loss is only logged after each epoch instead of step

--- src/llama_recipes/utils/train_utils.py
+++ src/llama_recipes/utils/train_utils.py
@@ -112,8 +112,9 @@ def train(model, train_dataloader,eval_dataloader, tokenizer, optimizer, lr_sche
                         optimizer.zero_grad()
                         pbar.update(1)
                     
-                wandb.log({"epoch": epoch, "step": step, "loss": loss.detach().float()})
                 pbar.set_description(f"Training Epoch: {epoch+1}/{train_config.num_epochs}, step {step}/{len(train_dataloader)} completed (loss: {loss.detach().float()})")
+                wandb.log({"epoch": epoch, "loss": loss.detach().float()})
+
             pbar.close()
                 
         epoch_end_time = time.perf_counter()-epoch_start_time
